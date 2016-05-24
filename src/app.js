aac1d2f14e23459a369258a2a26ae8e7c8bad246
Luqmaan <ldawoodjee@gmail.com>
2016-05-24 16:57:03 -0400

    Get headers working

--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
-function loadJson(path, callback) {
+function load(path) {
   return new Promise((resolve, reject) => {
     const req = new XMLHttpRequest();
     req.overrideMimeType('application/json');
     req.open('GET', path, true);
     req.onreadystatechange = function () {
       if (req.readyState == 4 && req.status == '200') {
-        resolve(JSON.parse(req.responseText));
+        resolve(req.responseText);
       }
     }
     req.send(null);
   });
 }
 
-const promiseWeapons = loadJson('/data/weapons.json')
-  .then((res) => {
-    const weaponsByName = window.weaponsByName = _.chain(res)
-      .map(w => _.set(w, 'name', w['More...']))
-      .filter(w => w['name'] !== '')
-      .keyBy('name')
-      .value();
-    return weaponsByName;
-  })
+function loadJson(path) {
+  return load(path).then((res) => JSON.parse(res));
+}
+
+const promiseWeapons = new Promise((resolve, reject) => {
+  d3.csv('/data/weapons.csv')
+    .row((d) => {
+      return _.chain(d)
+        .set('name', d['More...'])
+        .value();
+    })
+    .get((error, rows) => {
+      if (error) {
+        console.log(error)
+        reject(error);
+      }
+      window.rows = rows;
+      const weaponGroups = _.chain(rows)
+        .filter(r => !!r.name)
+        .keyBy('name')
+        .value();
+      resolve(weaponGroups);
+    });
+});
 
 const promiseWeaponGroups = loadJson('/data/weapon_groups.json')
   .then((res) => {
@@ -32,7 +47,10 @@ Promise.all([
   promiseWeapons,
   promiseWeaponGroups,
 ])
-.then(() => {
+.then((args) => {
+  const weaponsByName = args[0];
+  const weaponGroups = args[1];
+  console.log(weaponsByName)
   draw(weaponGroups.ar.map(name => weaponsByName[name]))
 });
 
@@ -49,7 +67,7 @@ function draw(weapons) {
         borderWidth: 1,
         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
         hoverBorderColor: 'rgba(255,99,132,1)',
-        data: weapons.map(w => w.Damage),
+        data: weapons.map(w => w['Damage::2']),
       }
     ]
   };
