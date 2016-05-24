4a48e5a4f64892d28a0b6e8b68e92c7b6c692a58
Luqmaan <ldawoodjee@gmail.com>
2016-05-24 14:29:13 -0400

    Flocka

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 function loadJson(path, callback) {
   return new Promise((resolve, reject) => {
     const req = new XMLHttpRequest();
-    req.overrideMimeType("application/json");
+    req.overrideMimeType('application/json');
     req.open('GET', path, true);
     req.onreadystatechange = function () {
-      if (req.readyState == 4 && req.status == "200") {
+      if (req.readyState == 4 && req.status == '200') {
         resolve(JSON.parse(req.responseText));
       }
     }
@@ -12,30 +12,49 @@ function loadJson(path, callback) {
   });
 }
 
-loadJson('/data/weapons.json')
-.then((res) => {
-  const weapons = window.weapons = _.chain(res)
-    .filter(w => w["More..."] !== "")
-    .value();
+const promiseWeapons = loadJson('/data/weapons.json')
+  .then((res) => {
+    const weaponsByName = window.weaponsByName = _.chain(res)
+      .map(w => _.set(w, 'name', w['More...']))
+      .filter(w => w['name'] !== '')
+      .keyBy('name')
+      .value();
+    return weaponsByName;
+  })
 
-  console.log('res', weapons, weapons[0]);
+const promiseWeaponGroups = loadJson('/data/weapon_groups.json')
+  .then((res) => {
+    const weaponGroups = window.weaponGroups = res;
+    return weaponGroups;
+  });
 
+Promise.all([
+  promiseWeapons,
+  promiseWeaponGroups,
+])
+.then(() => {
+  draw(weaponGroups.ar.map(name => weaponsByName[name]))
+});
+
+function draw(weapons) {
   const ctx = document.querySelector('#weapons-chart');
 
   const data = {
-    labels: [weapons.map(w => w['More...'])],
+    labels: weapons.map(w => w.name),
     datasets: [
       {
-        label: "My First dataset",
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        label: 'My First dataset',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
         borderWidth: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255,99,132,1)",
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
         data: weapons.map(w => w.Damage),
       }
     ]
   };
+
+  console.log('data', data)
 
   const options = {
     scales: {
@@ -53,5 +72,4 @@ loadJson('/data/weapons.json')
     data: data,
     options: options
   });
-
-});
+}
